ac49b5bc0b2c5bacd5403a3941707aeed8884824
Varun <mvresh@gmail.com>
2023-04-13 09:37:31 +0100

    new code

--- src/App.js
+++ src/App.js
@@ -52,36 +52,149 @@ const StyledMenu = styled((props) => (
 
 function App() {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl1, setAnchorEl1] = React.useState(null);
+  const [anchorEl2, setAnchorEl2] = React.useState(null);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [selectedOption1, setSelectedOption1] = useState("");
+  const [selectedOption2, setSelectedOption2] = useState("");
+  const [isSubmitEnabled, setIsSubmitEnabled] = useState(false);
+  const [showError, setShowError] = useState(false);
+  const [resultText, setResultText] = useState("");
+
+  const handleClose1 = (event) => {
+    setSelectedOption1(event.target.innerText);
+    setAnchorEl1(null);
+
+    setIsSubmitEnabled(() => {
+      return selectedOption2 !== "" && event.target.innerText !== selectedOption2;
+    });
+
+    // if (selectedOption2 !== "") {
+    //   setIsSubmitEnabled(true);
+    // }
+
+    // if (selectedOption1 === selectedOption2) {
+    //   setShowError(true);
+    //   setIsSubmitEnabled(false);
+    //   } else {
+    //   setShowError(false);
+    //   }
+  };
+  
+  const handleClose2 = (event) => {
+    setSelectedOption2(event.target.innerText);
+    setAnchorEl2(null);
+
+    setIsSubmitEnabled(() => {
+      return selectedOption1 !== "" && event.target.innerText !== selectedOption1;
+    });
+
+    // if (selectedOption1 !== "") {
+    //   setIsSubmitEnabled(true);
+    // }
+
+    // if (selectedOption1 === selectedOption2) {
+    //   setShowError(true);
+    //   setIsSubmitEnabled(false);
+    //   } else {
+    //   setShowError(false);
+    //   }
+  };
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
-  const handleClose = () => {
+  const handleClose = (event) => {
+    setSelectedOption(event.target.innerText);
     setAnchorEl(null);
   };
+  const open1 = Boolean(anchorEl1);
+  const handleClick1 = (event) => {
+    setAnchorEl1(event.currentTarget);
+  };
+  const open2 = Boolean(anchorEl2);
+  const handleClick2 = (event) => {
+    setAnchorEl2(event.currentTarget);
+  };
+
+  const submitHandler = () => {
+    if (selectedOption1 === selectedOption2) {
+      setShowError(true);
+      setIsSubmitEnabled(false);
+    } else {
+      setShowError(false);
+      setIsSubmitEnabled(true);
+      switch (selectedOption1) {
+        case "XLRE":
+          switch (selectedOption2) {
+            case "XLU":
+              setResultText("XLRE Avg. ESG score (1055) is less than XLU Avg. ESG score (1197)");
+              break;
+            case "ESGU":
+              setResultText("XLRE Avg. ESG score (1055) is way higher than SP ESG Avg. ESG score (114)");
+              break;
+            default:
+              setResultText("");
+              break;
+          }
+          break;
+        case "XLU":
+          switch (selectedOption2) {
+            case "XLRE":
+              setResultText("XLU Avg. ESG score (1197) is more than XLU Avg. ESG score (1055)");
+              break;
+            case "ESGU":
+              setResultText("XLU Avg. ESG score (1197) is way higher than SP ESG Avg. ESG score (114)");
+              break;
+            default:
+              setResultText("");
+              break;
+          }
+          break;
+        case "ESGU":
+          switch (selectedOption2) {
+            case "XLRE":
+              setResultText("SP ESG Avg. ESG score (114) is way less than XLRE Avg. ESG score (1055)");
+              break;
+            case "XLU":
+              setResultText("SP ESG Avg. ESG score (114) is way less than XLU Avg. ESG score (1197)");
+              break;
+            default:
+              setResultText("");
+              break;
+          }
+          break;
+        default:
+          setResultText("");
+          break;
+      }
+    }
+  };
+
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div className="dropdown-container">
-          <div className="dropdown">
-          <Button
-        id="demo-customized-button"
-        aria-controls={open ? 'demo-customized-menu' : undefined}
+        <h3 className="App-title" style={{ fontStyle: "italic", fontWeight: "normal" }}>
+    Compare ESG scores of ETF fund holdings in your portfolio
+        </h3>
+        <Button
+        id="demo-customized-button-3"
+        aria-controls={open ? 'demo-customized-menu-3' : undefined}
         aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
+        aria-expanded={open1 ? 'true' : undefined}
         variant="contained"
         disableElevation
         onClick={handleClick}
         endIcon={<KeyboardArrowDownIcon />}
       >
-        ETF Options
+        {selectedOption || "Source"}
       </Button>
       <StyledMenu
-        id="demo-customized-menu"
+        id="demo-customized-menu-3"
         MenuListProps={{
-          'aria-labelledby': 'demo-customized-button',
+          'aria-labelledby': 'demo-customized-button-3',
         }}
         anchorEl={anchorEl}
         open={open}
@@ -89,61 +202,132 @@ function App() {
       >
         <MenuItem onClick={handleClose} disableRipple>
           
+          ESG Enterprise
+        </MenuItem>
+        
+      </StyledMenu>
+      <div>
+          <p></p>
+        </div>
+        <div className="dropdown-container">
+          <div className="dropdown">
+          <Button
+        id="demo-customized-button-1"
+        aria-controls={open ? 'demo-customized-menu-1' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open1 ? 'true' : undefined}
+        variant="contained"
+        disableElevation
+        onClick={handleClick1}
+        endIcon={<KeyboardArrowDownIcon />}
+      >
+        {selectedOption1 || "ETF"}
+      </Button>
+      <StyledMenu
+        id="demo-customized-menu-1"
+        MenuListProps={{
+          'aria-labelledby': 'demo-customized-button-1',
+        }}
+        anchorEl={anchorEl1}
+        open={open1}
+        onClose={handleClose1}
+      >
+        <MenuItem onClick={handleClose1} disableRipple>
+          
           XLRE
         </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
+        <MenuItem onClick={handleClose1} disableRipple>
           
-          XE
+          XLU
         </MenuItem>
         {/* <Divider sx={{ my: 0.5 }} /> */}
-        <MenuItem onClick={handleClose} disableRipple>
+        <MenuItem onClick={handleClose1} disableRipple>
           
-          SP ESG
+          ESGU
         </MenuItem>
         
       </StyledMenu>
             </div>
             <div className="dropdown">
           <Button
-        id="demo-customized-button"
-        aria-controls={open ? 'demo-customized-menu' : undefined}
+        id="demo-customized-button-2"
+        aria-controls={open ? 'demo-customized-menu-2' : undefined}
         aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
+        aria-expanded={open2 ? 'true' : undefined}
         variant="contained"
         disableElevation
-        onClick={handleClick}
+        onClick={handleClick2}
         endIcon={<KeyboardArrowDownIcon />}
       >
-        ETF Options
+        {selectedOption2 || "ETF"}
       </Button>
       <StyledMenu
-        id="demo-customized-menu"
+        id="demo-customized-menu-2"
         MenuListProps={{
-          'aria-labelledby': 'demo-customized-button',
+          'aria-labelledby': 'demo-customized-button-2',
         }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
+        anchorEl={anchorEl2}
+        open={open2}
+        onClose={handleClose2}
       >
-        <MenuItem onClick={handleClose} disableRipple>
+        <MenuItem onClick={handleClose2} disableRipple>
           
           XLRE
         </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
+        <MenuItem onClick={handleClose2} disableRipple>
           
-          XE
+          XLU
         </MenuItem>
         {/* <Divider sx={{ my: 0.5 }} /> */}
-        <MenuItem onClick={handleClose} disableRipple>
+        <MenuItem onClick={handleClose2} disableRipple>
           
-          SP ESG
+          ESGU
         </MenuItem>
         
       </StyledMenu>
             </div>
         </div>
+        <div>
+          <p></p>
+        </div>
+        <div className="submit-button">
+            <Button
+            variant="contained"
+            disabled={!isSubmitEnabled}
+            onClick={submitHandler}
+            >
+            Submit
+          </Button>
+          </div>
+          <div>
+          <p></p>
+        </div>
+        {showError && (
+          <div className="error-text">
+            Please select different options.
+          </div>
+        )}
+        {resultText && (
+          <div className="result-text">
+            {resultText}
+          </div>
+        )}
+        <div>
+          <p></p>
+        </div>
+        <div>
+          <p></p>
+        </div>
+          <p style={{ fontSize: "14px", fontStyle: "bold" }}> ETFs available for comparison (as of 13th April 2023):<br />
+            </p>
+            <p style={{ fontSize: "10px"}}>
+            XLRE:   Real Estate Select Sector SPDR Fund<br />
+            XLU:  Utilities Select Sector SPDR Fund<br />
+            ESGU:  iShares ESG Aware MSCI USA ETF<br />
+          </p>
       </header>
     </div>
+    
   );
 }
 
